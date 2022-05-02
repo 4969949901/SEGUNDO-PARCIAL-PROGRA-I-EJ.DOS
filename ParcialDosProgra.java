e5a843421c129e697db872d9f1f76e0c8d741f02
Helen <lissa.acevedo99@gmail.com>
2022-05-01 20:43:15 -0600

    Se trabaja en la clase principal ya que tuve problemas para hacer el commit me toco reguardar el arhivo, agrege un switch para seleccion

--- ParcialDosProgra.java
+++ ParcialDosProgra.java
@@ -1,11 +1,38 @@
 
 package parcialdosprogra;
 
+import java.util.Scanner;
+
 public class ParcialDosProgra {
 
  
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        TarjetasBancariasPd padre= new TarjetasBancariasPd ();
+        TarjetaCreditoHj hija1= new TarjetaCreditoHj();
+        TarjetaDebitoHj hija2= new TarjetaDebitoHj();
+      
+        System.out.println("USO DE TARJETAS DE CREDITO/DEBITO\n");
+        System.out.println("A continuacion seleccione la tarjeta bancaria que utilizara");
+        System.out.println("1.Tarjeta de Credito");
+        System.out.println("2.Tarjeta de Debito");
+        System.out.println("3.Salir");
+        Scanner teclado= new Scanner (System.in);
+        int opc;
+        System.out.println("Introduzca el numero de opción por favor");
+        opc=teclado.nextInt();  
+        switch(opc ){
+            
+            case 1:
+                System.out.println("Ingreso a la opcion de Tarjeta de Crédito bienvenido");
+                System.out.println("Su limite de saldo es de: ");
+        
+                
+        }
+        
+        
+}
+    
+    
+    
     
 }
